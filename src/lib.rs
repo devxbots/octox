0317b4bf5bbe4b7a827705443e70667e5b8740bf
Jan David <jd@devxbots.com>
2022-05-25 12:41:38 +0200

    Configure GitHub App (#4)
    GitHub Apps require some configuration to be able to interact with the
    GitHub platform. At a minimum, the app id and private key must be
    provided so that the app can authenticate itself when making API calls.
    
    Optionally, a webhook secret can be configured to cryptographically sign
    the webhook payload and prevent tampering. While GitHub considers the
    webhook secret an optional feature, octox enforces that it is set to
    increase the security of its apps.

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,22 @@
 use std::fmt::{Display, Formatter};
+use std::fs::File;
+use std::io::Read;
 use std::net::{IpAddr, Ipv4Addr, SocketAddr, TcpListener};
 
 use axum::routing::get;
-use axum::{Router, Server};
+use axum::{Extension, Router, Server};
 
 pub use self::error::Error;
+pub use self::github::{AppId, PrivateKey, WebhookSecret};
 
 mod error;
+mod github;
 
 #[derive(Debug)]
 pub struct Octox {
+    app_id: Option<AppId>,
+    private_key: Option<PrivateKey>,
+    webhook_secret: Option<WebhookSecret>,
     address: SocketAddr,
     tcp_listener: Option<TcpListener>,
 }
@@ -17,6 +24,21 @@ pub struct Octox {
 impl Octox {
     pub fn new() -> Self {
         Self::default()
+    }
+
+    pub fn app_id(mut self, app_id: u64) -> Result<Self, Error> {
+        self.app_id = Some(AppId::new(app_id));
+        Ok(self)
+    }
+
+    pub fn private_key(mut self, private_key: &str) -> Result<Self, Error> {
+        self.private_key = Some(PrivateKey::new(private_key.into()));
+        Ok(self)
+    }
+
+    pub fn webhook_secret(mut self, webhook_secret: &str) -> Result<Self, Error> {
+        self.webhook_secret = Some(WebhookSecret::new(webhook_secret.into()));
+        Ok(self)
     }
 
     pub fn address(mut self, address: SocketAddr) -> Result<Self, Error> {
@@ -32,7 +54,11 @@ impl Octox {
     }
 
     pub async fn serve(self) -> Result<(), Error> {
-        let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+        let app = Router::new()
+            .route("/", get(|| async { "Hello, World!" }))
+            .layer(self.app_id_extension()?)
+            .layer(self.private_key_extension()?)
+            .layer(self.webhook_secret_extension()?);
 
         let listener = match self.tcp_listener {
             Some(listener) => listener,
@@ -45,6 +71,65 @@ impl Octox {
 
         Ok(())
     }
+
+    fn app_id_extension(&self) -> Result<Extension<AppId>, Error> {
+        if let Some(app_id) = self.app_id {
+            return Ok(Extension(app_id));
+        }
+
+        let app_id_from_env = match std::env::var("OCTOX_APP_ID") {
+            Ok(app_id) => app_id,
+            Err(_) => {
+                return Err(Error::Configuration(
+                    "app id must be set either manually or as an environment variable".into(),
+                ))
+            }
+        };
+
+        if let Ok(app_id) = app_id_from_env.parse::<u64>() {
+            return Ok(Extension(AppId::new(app_id)));
+        }
+
+        Err(Error::Configuration("app id must be a number".into()))
+    }
+
+    fn private_key_extension(&self) -> Result<Extension<PrivateKey>, Error> {
+        if let Some(private_key) = &self.private_key {
+            return Ok(Extension(private_key.clone()));
+        }
+
+        if let Ok(private_key) = std::env::var("OCTOX_PRIVATE_KEY") {
+            return Ok(Extension(PrivateKey::new(private_key)));
+        };
+
+        let private_key_path = match std::env::var("OCTOX_PRIVATE_KEY_PATH") {
+            Ok(path) => path,
+            Err(_) => {
+                return Err(Error::Configuration(
+                    "private key must be set either manually or as an environment variable".into(),
+                ))
+            }
+        };
+
+        let mut private_key = String::new();
+        File::open(private_key_path)?.read_to_string(&mut private_key)?;
+
+        Ok(Extension(PrivateKey::new(private_key)))
+    }
+
+    fn webhook_secret_extension(&self) -> Result<Extension<WebhookSecret>, Error> {
+        if let Some(webhook_secret) = &self.webhook_secret {
+            return Ok(Extension(webhook_secret.clone()));
+        }
+
+        if let Ok(webhook_secret) = std::env::var("OCTOX_WEBHOOK_SECRET") {
+            return Ok(Extension(WebhookSecret::new(webhook_secret)));
+        }
+
+        Err(Error::Configuration(
+            "webhook secret must be set either manually or as an environment variable".into(),
+        ))
+    }
 }
 
 impl Display for Octox {
@@ -56,6 +141,9 @@ impl Display for Octox {
 impl Default for Octox {
     fn default() -> Self {
         Self {
+            app_id: None,
+            private_key: None,
+            webhook_secret: None,
             address: SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 3000),
             tcp_listener: None,
         }
@@ -76,13 +164,42 @@ mod tests {
     }
 
     #[test]
+    fn app_id_sets_app_id() -> Result<(), Error> {
+        let octox = Octox::new();
+
+        let octox = octox.app_id(1)?;
+
+        assert!(octox.app_id.is_some());
+        Ok(())
+    }
+
+    #[test]
+    fn private_key_sets_private_key() -> Result<(), Error> {
+        let octox = Octox::new();
+
+        let octox = octox.private_key("private_key")?;
+
+        assert!(octox.private_key.is_some());
+        Ok(())
+    }
+
+    #[test]
+    fn webhook_secet_sets_webhook_secret() -> Result<(), Error> {
+        let octox = Octox::new();
+
+        let octox = octox.webhook_secret("webhook_secret")?;
+
+        assert!(octox.webhook_secret.is_some());
+        Ok(())
+    }
+
+    #[test]
     fn address_sets_address() -> Result<(), Error> {
         let octox = Octox::new();
 
         let octox = octox.address("127.0.0.1:8000".parse::<SocketAddr>().unwrap())?;
 
         assert_eq!("127.0.0.1:8000", octox.address.to_string());
-
         Ok(())
     }
 
@@ -97,7 +214,6 @@ mod tests {
 
         assert_eq!("127.0.0.1:8000", octox.address.to_string());
         assert!(octox.tcp_listener.is_none());
-
         Ok(())
     }
 
@@ -111,7 +227,6 @@ mod tests {
         let octox = octox.listener(tcp_listener)?;
 
         assert_eq!(address, octox.address);
-
         Ok(())
     }
 }
